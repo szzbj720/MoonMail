7cd6af6cd103076696ddbab158350230daf87e07
szzbj720 <arb8fd@virginia.edu>
2026-05-20 15:17:57 -0400

    Initial Commit

---
+++ MoonMail/MoonMailApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonMailApp.swift
+//  MoonMail
+//
+//  Created by Selena Zhang on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct MoonMailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
